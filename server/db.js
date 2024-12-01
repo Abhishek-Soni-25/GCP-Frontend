e77f8efa1b14b77c0aa5bce1dbb8d7d9abeff8aa
Abhishek-Soni-25 <abhi250604@gmail.com>
2024-12-01 17:16:01 +0530

    Edited DB credentials

--- server/db.js
+++ server/db.js
@@ -7,12 +7,12 @@ const path = require("path");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME, //'gcd_frontend',  // Database name (hardcoded)
-  process.env.DB_USER, //'root',           // Database user (hardcoded)
-  process.env.DB_PASSWORD, //'9069076975',     // Database password (hardcoded)
+  process.env.DB_NAME, 
+  process.env.DB_USER, 
+  process.env.DB_PASSWORD, 
   {
-    host: process.env.DB_HOST, //'localhost',  // Database host (hardcoded)
-    dialect: "mysql", // Dialect (MySQL)
+    host: process.env.DB_HOST,
+    dialect: "mysql", 
   }
 );
 
